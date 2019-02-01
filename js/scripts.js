1176f3657b2ce207aa6d3f32d846a8ba05716ecc
Doris Warren <doriswarren11@gmail.com>
2019-02-01 12:32:58 -0800

    added var to calculate total cost of toppings

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,29 @@ OrderTotal.protoype.addPizzaToOrder = function(pizza) {
   this.pizzaCost = pizza.toppingCost(pizza);
   this.pizzas.push(pizza);
 }
+
+function Pizza (toppings,size) {
+  this.toppings = toppings;
+  this.size = size;
+}
+
+Pizza.prototype.pizzaToppingCost = function(toppingsCostArray) {
+  this.toppingCost = toppingTotal;
+  var toppingTotal = 0;
+  for (var i = 0; i < toppingsCostArray.length; i++) {
+    toppingTotal += toppingsCostArray[i];
+}
+  return toppingTotal + this.size;
+}
+
+//user logic
+
+$(document).ready(function) {
+$("orderForm").submit(function(event){
+  event.preventDefault();
+
+var toppingsInputCostsArray = [];
+$("input:;checkbox[name=topping]:checked").each(function() {
+  var toppingsInputCost =
+})
+});
